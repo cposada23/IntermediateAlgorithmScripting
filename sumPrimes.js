f27a325ced2ad6ddc03841fb9054c41961ef879b
Camilo <cposada@gmail.com>
2016-07-15 22:32:52 -0500

    suma primes

---
+++ sumPrimes.js
@@ -0,0 +1,22 @@
+
+function sumPrimes(num) {
+
+    var sum = 0;
+    for(var i = 1; i <=num; i++){
+        var contador = 0;
+        console.log(i);
+        for (var j = 1; j<=i; j++){
+            if(i%j===0){
+                contador ++;
+            }
+        }
+        if(contador === 2){
+            console.log("si");
+            sum +=i;
+        }
+
+    }
+    return sum;
+}
+
+console.log(sumPrimes(10));
